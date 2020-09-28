8162ef36b6de87adc79a2ced7ffeba3ea02dacbc
shreyas192007 <70263216+shreyas192007@users.noreply.github.com>
2020-09-28 17:43:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var ball;
+var database,ballposition,position;
+
+function setup(){
+
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    ballposition=database.ref('ball/position');
+    ballposition.on("value",readposition,showerror) 
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writeposition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writeposition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writeposition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writeposition(0,+1);
+    }
+    drawSprites();
+}
+
+function writeposition(x,y){
+database.ref('ball/position').set({
+     'x':position.x+x,
+     'y':position.y+y
+})
+
+}
+
+/*function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}*/
+
+function readposition(data){
+
+position=data.val();
+ball.x=position.x;
+ball.y=position.y;
+
+
+}
+
+function showerror(){
+
+console.log("error writing the database");
+
+}
